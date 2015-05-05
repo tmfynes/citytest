1d4350ee07c0c397803ce6a0deb1c003eedaf192
tmfynes <tmfynes@yahoo.com>
2015-05-05 16:26:26 -0400

    test

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Everyone @ ZipCar";
+        return "Greetings Everyone @ ZipCar again";
     }
 }
